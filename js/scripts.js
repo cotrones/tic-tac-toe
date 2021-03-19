066909a5eca22ecfecb415610d1bba3921e80bab
cotrones <tomcotroneo@gmail.com>
2021-03-19 12:11:31 -0400

    refactor gameBoard module, move renderGameBoard to displayController

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,52 @@
 const gameBoard = (() => {
     const _gameBoard = new Array(9);
-    const container = document.querySelector('#gameBoard');
+
+    const getBoard = () => {
+        return _gameBoard;
+    }
+
+    const getValue = index => {
+        return _gameBoard[index];
+    }
+
+    const setValue = (index, value) => {
+        _gameBoard[index] = value;
+    }
+
+    const clearBoard = () => {
+        for (let i = 0; i < 9; i++) {
+            _gameBoard[i] = "";
+        }
+    }
+
+    return {
+        getBoard,
+        getValue,
+        setValue,
+        clearBoard
+    }
+})();
+
+const displayController = (() => {
+    const squares = document.querySelectorAll('.square');
 
     const renderGameBoard = (() => {
-        for (let [key, value] of _gameBoard.entries()) {
+        const board = gameBoard.getBoard();
+        for (let [key, value] of board.entries()) {
+            const container = document.querySelector('#gameBoard');
             const boardSquare = document.createElement('div');
+
             boardSquare.dataset.index = key;
             boardSquare.classList.add('square');
             container.append(boardSquare);
         }
     })();
 
-    return {
-        renderGameBoard
-    }
-})();
-
-const displayController = (() => {
+    squares.forEach(square => {
+        square.addEventListener('click', event => {
+            console.log(square.dataset.index);
+        });
+    });
 
     return {
         
