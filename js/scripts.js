3ab98f51773ce49b6548d2467da2ed059608a8d2
cotrones <tomcotroneo@gmail.com>
2021-03-19 08:42:22 -0400

    render and style gameBoard

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,32 @@
-console.log('Hello World');
+const gameBoard = (() => {
+    const _gameBoard = new Array(9);
+    const container = document.querySelector('#gameBoard');
+
+    const renderGameBoard = (() => {
+        for (let [key, value] of _gameBoard.entries()) {
+            const boardSquare = document.createElement('div');
+            boardSquare.dataset.index = key;
+            boardSquare.classList.add('gameSquare');
+            container.append(boardSquare);
+            console.log(key);
+        }
+    })();
+
+    return {
+        renderGameBoard
+    }
+})();
+
+const displayController = (() => {
+
+    return {
+        
+    }
+})();
+
+const player = () => {
+
+    return {
+
+    }
+}
